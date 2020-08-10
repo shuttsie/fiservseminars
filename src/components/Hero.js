4834793696406fa64ddee927259fe6ff1959adfd
rstrasser04 <69007424+rstrasser04@users.noreply.github.com>
2020-08-10 15:28:21 -0500

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,7 +7,7 @@ const Hero = () => (
   <div className="text-center hero my-5">
     <img className="mb-3 app-logo" src={logo} alt="React logo" width="962px" />
     <h1 className="mb-4">Sharing Knowledge & Making Connections. <span style={{color: "#000",fontWeight: "normal"}}>That's what the seminars are all about!</span></h1>
-    <h2><i>Guiding growth, delivering excellence.</i></h2>
+    <h2><i>Guiding Growth, Delivering, and Excellence.</i></h2>
     <HeroInfo />
   </div>
 );
