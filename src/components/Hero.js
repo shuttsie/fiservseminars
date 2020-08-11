eb828e25bddea5e17f533f016156903878ba5f17
strasserra04 <38735391+strasserra04@users.noreply.github.com>
2020-08-11 09:13:10 -0500

    updated CSS

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,7 +5,7 @@ import logo from "../assets/header.png";
 
 const Hero = () => (
   <div className="text-center hero my-5">
-    <img className="mb-3 app-logo" src={logo} alt="React logo" width="962px" />
+    <img className="mb-3 app-logo" src={logo} alt="React logo" width="" />
     <h1 className="mb-4">Guiding Growth, Delivering Excellence. <span style={{color: "#000",fontWeight: "normal"}}>That's what the seminars are all about!</span></h1>
     <HeroInfo />
   </div>
