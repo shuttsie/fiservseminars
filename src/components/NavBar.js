cff866519f221810bd044515c40862be00d278db
strasserra04 <38735391+strasserra04@users.noreply.github.com>
2020-08-11 15:29:03 -0500

    fixed flex issue

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -150,7 +150,7 @@ const NavBar = () => {
                     block
                     onClick={() => loginWithRedirect({})}
                   >
-                    Log in
+                    Register
                   </Button>
                 </NavItem>
               </Nav>
