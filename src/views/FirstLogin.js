3266f288f2898302dcbae56330054e7c96877a35
strasserra04 <38735391+strasserra04@users.noreply.github.com>
2020-08-14 11:18:04 -0500

    updating First Loging screen

--- src/views/FirstLogin.js
+++ src/views/FirstLogin.js
@@ -4,12 +4,12 @@ class FirstLogin extends Component {
   render() {
     return (
       <div className="next-steps my-5 seminar-container">
-          <h2>Thank you registering!</h2>
+          <h2>Thank you for registering for the Precision Education Seminar!</h2>
           <p class="italic-important">
-            We are excited you can join us for this exciting seminar.
+            We are thrilled you can join us for our three day virtual seminar.
           </p>
           <p>
-            Please watch out for a registration confirmation email shortly.
+            Please watch out for a registration confirmation email shortly. Once you recieve that, please verify your account to be able to log in the first time.
           </p>
       </div>
     );
