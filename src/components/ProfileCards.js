75b1d7708827bbcb6d30f512d9d72d836a3bfdbb
strasserra04 <38735391+strasserra04@users.noreply.github.com>
2020-09-29 08:21:18 -0500

    Updates to ProfileCards and styles

--- src/components/ProfileCards.js
+++ src/components/ProfileCards.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Card, Icon, Image } from 'semantic-ui-react'
 import Loading from '../components/Loading'
 import { useAuth0, withAuthenticationRequired } from '@auth0/auth0-react'
