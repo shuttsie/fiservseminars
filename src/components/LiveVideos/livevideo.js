f87c5d8a25e59e434779f0f44a7d5ece9dea0bf4
strasserra04 <38735391+strasserra04@users.noreply.github.com>
2020-10-06 11:00:33 -0500

    Updated vido to Video.js

--- src/components/LiveVideos/livevideo.js
+++ src/components/LiveVideos/livevideo.js
@@ -1,95 +1,19 @@
-import React, { useEffect, useRef, useState } from 'react';
-import StreamPlayer from './streamplayer';
-import './player.css';
+import React, {useEffect, useRef} from 'react';
+import {Helmet} from "react-helmet";
 
-const feedJSON = `${process.env.PUBLIC_URL}/feed.json`;
+function LiveVideo() {
 
-function LiveVideo () {
-    
-    const { IVSPlayer } = window;
-    const { isPlayerSupported } = IVSPlayer;
-  
-    const [streams, setStreams] = useState([]);
-  
-    const [activeStreamId, setActiveStreamId] = useState();
-    const [loading, setLoading] = useState(false);
-    const [visibleVideos, setVisibleVideos] = useState([]);
-
-    const player = useRef(null);
-    useEffect(() => {
-        async function fetchStreams() {
-          const response = await fetch(feedJSON);
-          const data = await response.json();
-    
-          setStreams(data.streams);
-        }
-    
-        fetchStreams();
-      }, []);
-  useEffect(() => {
-    const { ENDED, PLAYING, READY } = IVSPlayer.PlayerState;
-    const { ERROR } = IVSPlayer.PlayerEventType;
-
-    if (!isPlayerSupported) {
-      console.warn(
-        'The current browser does not support the Amazon IVS player.',
-      );
-
-      return;
-    }
-
-    const onStateChange = () => {
-      const newState = player.current.getState();
-
-      console.log(`Player State - ${newState}`);
-
-      setLoading(newState !== PLAYING);
-    };
-
-    const onError = (err) => {
-      console.warn('Player Event - ERROR:', err);
-    };
-
-    player.current = IVSPlayer.create();
-
-    player.current.addEventListener(READY, onStateChange);
-    player.current.addEventListener(PLAYING, onStateChange);
-    player.current.addEventListener(ENDED, onStateChange);
-    player.current.addEventListener(ERROR, onError);
-
-    return () => {
-      player.current.removeEventListener(READY, onStateChange);
-      player.current.removeEventListener(PLAYING, onStateChange);
-      player.current.removeEventListener(ENDED, onStateChange);
-      player.current.removeEventListener(ERROR, onError);
-    };
-  }, [IVSPlayer, isPlayerSupported]);
-
-  const setStream = (id, visible) => {
-    const index = visibleVideos.indexOf(id);
-
-    if (index > -1 && visible) return;
-
-    let videos = [...visibleVideos];
-
-    if (visible) {
-      videos.push(id);
-    } else {
-      videos.splice(index, 1);
-    }
-
-    setLoading(true);
-    setVisibleVideos(videos);
-    setActiveStreamId(videos[videos.length - 1]);
-  };
-
-  if (!isPlayerSupported) {
-    return null;
-  }
     return (
-      <div className="App full-width full-height">
-        <StreamPlayer />
-    </div>
+      <div>
+        <div>
+          <Helmet>
+            <script src="https://fiservseminars-media.s3.amazonaws.com/Videos/video-script.js" type="text/javascript" />
+          </Helmet>
+        </div>
+        <div className="video-container">
+          <video id="amazon-ivs-videojs" className="video-js vjs-4-3 vjs-big-play-centered" controls autoPlay></video>
+        </div>
+      </div>
     );
 }
 
---
+++ src/video-script.js
@@ -0,0 +1,16 @@
+(function play() {
+    // Get playback URL from Amazon IVS API
+    var PLAYBACK_URL = 'https://ca351d1575ab.us-east-1.playback.live-video.net/api/video/v1/us-east-1.076005434014.channel.bQ1l9LwPytZA.m3u8';
+    
+    // Register Amazon IVS as playback technology for Video.js
+    registerIVSTech(videojs);
+
+    // Initialize player
+    var player = videojs('amazon-ivs-videojs', {
+        techOrder: ["AmazonIVS"]
+    }, () => {
+        console.log('Player is ready to use!');
+        // Play stream
+        player.src(PLAYBACK_URL);
+    });
+})();
